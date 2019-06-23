3deac5f224305eee28af6d34612acb0e7a03649a
IamAniruth <prakashgovind4@gmail.com>
2019-06-23 21:29:42 +0530

    1 file modified

--- Container/Home.js
+++ Container/Home.js
@@ -3,6 +3,7 @@ import { withStyles  } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const useStyles = theme => ({
   root: {
@@ -14,6 +15,9 @@ const useStyles = theme => ({
   },
   control: {
     padding: theme.spacing(2),
+  },
+   button: {
+    margin: theme.spacing(1),
   },
 });
 
@@ -30,7 +34,9 @@ class Home extends Component{
       direction="row"
       justify="center"
       alignItems="center"
-       item xs={12}>
+       item xs={12}
+      
+       >
        <Typography variant="h1" component="h2" gutterBottom style={{padding:'30px'}}>
      <b> Innovation story Landing </b>
       </Typography>
@@ -44,11 +50,377 @@ class Home extends Component{
        <Typography variant="h6" component="h6" gutterBottom style={{padding:'30px',margin:'0px 50px 120px 120px'}}>
       These cookies are used to collect information about how you interact with our website and allow us to remember you. We use this information in order to improve and customize your browsing experience and for analytics and metrics about our visitors both on this website and other media
       </Typography>
+
       </Grid>
+       
        <hr />
       </div>
+      <Grid container
+  direction="row"
+  justify="space-around"
+  alignItems="center" xs={12}  style={{marginBottom:'50px'}} >
+         <Grid item xs={3}>
+          <Grid item xs={12}>
+          <img src={'https://cdn.pixabay.com/photo/2018/04/11/19/48/player-3311600_960_720.png'}  height="100%" width="100%" />
+          </Grid>
+           <Grid item xs={12}>
+           <div>
+            These cookies are used to collect information about how you interact with our website and allow us to remember you. We use this information in order to improve and customize your browsing experience and for analytics and metrics about our visitors both on this website and other media
+            </div>
+            <div>
+            <Button variant="contained" className={classes.button}>
+             View full story
+          </Button>
+            </div>
+          </Grid>
+         </Grid>
+         <Grid item xs={3}>
+          <Grid item xs={12}>
+          <img src={'https://cdn.pixabay.com/photo/2018/04/11/19/48/player-3311600_960_720.png'}  height="100%" width="100%" />
+          </Grid>
+           <Grid item xs={12}>
+          <div>
+            These cookies are used to collect information about how you interact with our website and allow us to remember you. We use this information in order to improve and customize your browsing experience and for analytics and metrics about our visitors both on this website and other media
+            </div>
+            <div>
+            <Button variant="contained" className={classes.button}>
+             View full story
+          </Button>
+            </div>
+          </Grid>
+         </Grid>
+           <Grid item xs={3}>
+          <Grid item xs={12}>
+          <img src={'https://cdn.pixabay.com/photo/2018/04/11/19/48/player-3311600_960_720.png'}  height="100%" width="100%" />
+          </Grid>
+           <Grid item xs={12}>
+           <div>
+            These cookies are used to collect information about how you interact with our website and allow us to remember you. We use this information in order to improve and customize your browsing experience and for analytics and metrics about our visitors both on this website and other media
+            </div>
+            <div>
+            <Button variant="contained" className={classes.button}>
+             View full story
+          </Button>
+            </div>
+          </Grid>
+         </Grid>
+          
+         
       </Grid>
 
+      <Grid container
+  direction="row"
+  justify="space-around"
+  alignItems="center" xs={12} style={{marginBottom:'50px'}} >
+         <Grid item xs={3}>
+          <Grid item xs={12}>
+          <img src={'https://cdn.pixabay.com/photo/2018/04/11/19/48/player-3311600_960_720.png'}  height="100%" width="100%" />
+          </Grid>
+           <Grid item xs={12}>
+           <div>
+            These cookies are used to collect information about how you interact with our website and allow us to remember you. We use this information in order to improve and customize your browsing experience and for analytics and metrics about our visitors both on this website and other media
+            </div>
+            <div>
+            <Button variant="contained" className={classes.button}>
+             View full story
+          </Button>
+            </div>
+          </Grid>
+         </Grid>
+         <Grid item xs={3}>
+          <Grid item xs={12}>
+          <img src={'https://cdn.pixabay.com/photo/2018/04/11/19/48/player-3311600_960_720.png'}  height="100%" width="100%" />
+          </Grid>
+           <Grid item xs={12}>
+           <div>
+            These cookies are used to collect information about how you interact with our website and allow us to remember you. We use this information in order to improve and customize your browsing experience and for analytics and metrics about our visitors both on this website and other media
+            </div>
+            <div>
+            <Button variant="contained" className={classes.button}>
+             View full story
+          </Button>
+            </div>
+          </Grid>
+         </Grid>
+           <Grid item xs={3}>
+          <Grid item xs={12}>
+          <img src={'https://cdn.pixabay.com/photo/2018/04/11/19/48/player-3311600_960_720.png'}  height="100%" width="100%" />
+          </Grid>
+           <Grid item xs={12}>
+           <div>
+            These cookies are used to collect information about how you interact with our website and allow us to remember you. We use this information in order to improve and customize your browsing experience and for analytics and metrics about our visitors both on this website and other media
+            </div>
+            <div>
+            <Button variant="contained" className={classes.button}>
+             View full story
+          </Button>
+            </div>
+          </Grid>
+         </Grid>
+          
+         
+      </Grid>
+      
+      </Grid>
+<hr />
+
+    <Grid
+    container
+    direction="row"
+    justify="space-around"
+    alignItems="center"
+    wrap
+  >
+
+  <Grid xs={4}  container
+    direction="row"
+     justify="space-around"
+    alignItems="center"
+    wrap
+    >
+  <Grid xs={3}>
+  <div style={{marginLeft:'25px',marginBottom:'25px'}} >
+  <div>
+   <img src={'https://cdn.pixabay.com/photo/2018/04/11/19/48/player-3311600_960_720.png'} height="100%" width="100%" />
+   </div>
+   <div>
+    <Button variant="contained" className={classes.button} >
+             View
+          </Button>
+   </div>
+   </div>
+  </Grid>
+  <Grid xs={7}>
+  <div>
+  <div>
+  <b> Tire 2 story Headline </b>
+  </div>
+  <div>
+   These cookies are used to collect information about how you interact with our website 
+   </div>
+   </div>
+  </Grid>
+  </Grid>
+
+    <Grid xs={4}  container
+    direction="row"
+     justify="space-around"
+    alignItems="center"
+    wrap>
+  <Grid xs={3}>
+     <div style={{marginLeft:'25px'}} >
+  <div>
+   <img src={'https://cdn.pixabay.com/photo/2018/04/11/19/48/player-3311600_960_720.png'} height="100%" width="100%" />
+   </div>
+   <div>
+    <Button variant="contained" className={classes.button}>
+             View
+          </Button>
+   </div>
+   </div>
+  </Grid>
+  <Grid xs={7}>
+  <div>
+  <div>
+  <b> Tire 2 story Headline </b>
+  </div>
+  <div>
+   These cookies are used to collect information about how you interact with our website 
+   </div>
+   </div>
+  </Grid>
+  </Grid>
+
+    <Grid xs={4}  container
+    direction="row"
+     justify="space-around"
+    alignItems="center"
+    wrap>
+  <Grid xs={3}>
+    <div style={{marginLeft:'25px'}} >
+  <div>
+   <img src={'https://cdn.pixabay.com/photo/2018/04/11/19/48/player-3311600_960_720.png'} height="100%" width="100%" />
+   </div>
+   <div>
+    <Button variant="contained" className={classes.button}>
+             View
+          </Button>
+   </div>
+   </div>
+  </Grid>
+  <Grid xs={7}>
+  <div>
+  <div>
+  <b> Tire 2 story Headline </b>
+  </div>
+  <div>
+   These cookies are used to collect information about how you interact with our website 
+   </div>
+   </div>
+  </Grid>
+  </Grid>
+
+     <Grid xs={4}  container
+    direction="row"
+     justify="space-around"
+    alignItems="center"
+    wrap>
+  <Grid xs={3}>
+    <div style={{marginLeft:'25px'}} >
+  <div>
+   <img src={'https://cdn.pixabay.com/photo/2018/04/11/19/48/player-3311600_960_720.png'} height="100%" width="100%" />
+   </div>
+   <div>
+    <Button variant="contained" className={classes.button}>
+             View
+          </Button>
+   </div>
+   </div>
+  </Grid>
+  <Grid xs={7}>
+  <div>
+  <div>
+  <b> Tire 2 story Headline </b>
+  </div>
+  <div>
+   These cookies are used to collect information about how you interact with our website 
+   </div>
+   </div>
+  </Grid>
+  </Grid>
+
+     <Grid xs={4}  container
+    direction="row"
+     justify="space-around"
+    alignItems="center"
+    wrap>
+  <Grid xs={3}>
+    <div style={{marginLeft:'25px'}} >
+  <div>
+   <img src={'https://cdn.pixabay.com/photo/2018/04/11/19/48/player-3311600_960_720.png'} height="100%" width="100%" />
+   </div>
+   <div>
+    <Button variant="contained" className={classes.button}>
+             View
+          </Button>
+   </div>
+   </div>
+  </Grid>
+  <Grid xs={7}>
+  <div>
+  <div>
+  <b> Tire 2 story Headline </b>
+  </div>
+  <div>
+   These cookies are used to collect information about how you interact with our website 
+   </div>
+   </div>
+  </Grid>
+  </Grid>
+
+     <Grid xs={4}  container
+    direction="row"
+     justify="space-around"
+    alignItems="center"
+    wrap>
+  <Grid xs={3}>
+    <div style={{marginLeft:'25px'}} >
+  <div>
+   <img src={'https://cdn.pixabay.com/photo/2018/04/11/19/48/player-3311600_960_720.png'} height="100%" width="100%" />
+   </div>
+   <div>
+    <Button variant="contained" className={classes.button}>
+             View
+          </Button>
+   </div>
+   </div>
+  </Grid>
+  <Grid xs={7}>
+  <div>
+  <div>
+  <b> Tire 2 story Headline </b>
+  </div>
+  <div>
+   These cookies are used to collect information about how you interact with our website 
+   </div>
+   </div>
+  </Grid>
+  </Grid>
+
+     <Grid xs={4}  container
+    direction="row"
+     justify="space-around"
+    alignItems="center"
+    wrap>
+  <Grid xs={3}>
+     <div style={{marginLeft:'25px'}} >
+  <div>
+   <img src={'https://cdn.pixabay.com/photo/2018/04/11/19/48/player-3311600_960_720.png'} height="100%" width="100%" />
+   </div>
+   <div>
+    <Button variant="contained" className={classes.button}>
+             View
+          </Button>
+   </div>
+   </div>
+  </Grid>
+  <Grid xs={7}>
+  <div>
+  <div>
+  <b> Tire 2 story Headline </b>
+  </div>
+  <div>
+   These cookies are used to collect information about how you interact with our website 
+   </div>
+   </div>
+  </Grid>
+  </Grid>
+     <Grid xs={4}  container
+    direction="row"
+     justify="space-around"
+    alignItems="center"
+    wrap>
+
+  <Grid xs={10}>
+  <div>
+  <div>
+  <b> Tire 3 story Headline </b>
+  </div>
+  <div>
+   These cookies are used to collect information about how you interact with our website 
+   </div>
+      <div>
+    <Button variant="contained" className={classes.button}>
+             View
+          </Button>
+   </div>
+   </div>
+  </Grid>
+  </Grid>
+
+       <Grid xs={4}  container
+    direction="row"
+     justify="space-around"
+    alignItems="center"
+    wrap>
+
+  <Grid xs={10}>
+  <div>
+  <div>
+  <b> Tire 3 story Headline </b>
+  </div>
+  <div>
+   These cookies are used to collect information about how you interact with our website 
+   </div>
+      <div>
+    <Button variant="contained" className={classes.button}>
+             View
+          </Button>
+   </div>
+   </div>
+  </Grid>
+  </Grid>
+  </Grid>
      
       </div>
     )
